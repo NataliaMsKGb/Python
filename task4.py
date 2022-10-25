0016ebd75bac390c99ed3ab9869f9f41ff7fa298
Natalia <natusik_kem@list.ru>
2022-10-25 20:59:52 +0300

    dz

--- task4.py
+++ task4.py
@@ -1,1 +1,11 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных. Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+
+def show(array: [list]):
+    print('|', end=' ')
+print(*range(len(array)), sep=' | ', end=' | \n')
+print('—————————————')
+for i, row in enumerate(array):
+    print('|', end=' ')
+print(*row, sep=' | ', end=f' | {i}\n')
+print('—————————————')
